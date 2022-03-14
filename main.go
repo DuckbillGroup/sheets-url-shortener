f637cdc145fc63135566e554921f241a067617e0
Corey Quinn <corey@sequestered.net>
2022-03-14 11:54:04 -0700

    Temp redirects, lowercase

--- main.go
+++ main.go
@@ -134,11 +134,11 @@ func (s *server) redirect(w http.ResponseWriter, req *http.Request) {
 	}
 
 	log.Printf("redirecting=%q to=%q", req.URL, redirTo.String())
-	http.Redirect(w, req, redirTo.String(), http.StatusMovedPermanently)
+	http.Redirect(w, req, redirTo.String(), http.StatusFound)
 }
 
 func (s *server) findRedirect(req *url.URL) (*url.URL, error) {
-	path := strings.TrimPrefix(req.Path, "/")
+	path := strings.ToLower(strings.TrimPrefix(req.Path, "/"))
 
 	segments := strings.Split(path, "/")
 	var discard []string
